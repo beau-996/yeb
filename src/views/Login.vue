97a43505a3fe707925fa2a5f0ae70ed9dcfa2f47
mb <cj679712@gmail.com>
2022-12-28 02:44:29 +0800

    ty

---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-form ref="form" v-model="loginForm" class="loginContainer">
+      <h3>系统登录</h3>
+      <el-form-item>
+        <el-input type="text" auto-complete="false" v-model="loginForm.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="password" auto-complete="false" v-model="loginForm.password" placeholder="请输入密码">
+        </el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="text" auto-complete="false" v-model="loginForm.code" placeholder="点击图片更换验证码">
+          <img src="captcha">
+        </el-input>
+      </el-form-item>
+      <el-checkbox v-model="checked" class="">记住我</el-checkbox>
+      <el-button type="primary" style="width: 100%">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data(){
+    return{
+      loginForm:{
+        username:'admin',
+        password:'123',
+        code:""
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
